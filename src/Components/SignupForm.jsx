2789b9746bbc4f82f68c6385427164be08fafb73
arreich-ewu <arreich@eagles.ewu.edu>
2021-04-28 12:47:51 +0200

    template of form

--- src/Components/SignupForm.jsx
+++ src/Components/SignupForm.jsx
@@ -1,2 +1,35 @@
 import React from 'react';
 
+const SignupForm = () => {
+    return (
+        <form>
+            <div className='form-group'>
+                <label for="title">Title: *&nbsp;</label>
+                <input type="select" required />
+            </div>
+            <div className='form-group'>
+                <label for="fname">First Name: *&nbsp;</label>
+                <input name="fname" type="text" required />
+            </div>
+            <div className='form-group'>
+                <label for="lname">Last Name: *&nbsp;</label>
+                <input name="lname" type="text" required />
+            </div>
+            <div className='form-group'>
+                <label for="email">Email: *&nbsp;</label>
+                <input name="email" type="email" required />
+            </div>
+            <div className='form-group'>
+                <label for="dob">Date of Birth:&nbsp;</label>
+                <input name="dob" type="date" />
+            </div>
+            <div className='form-group'>
+                <label for="dob">Date of Birth:&nbsp;</label>
+                <input name="dob" type="tel" />
+            </div>
+
+        </form>
+    );
+};
+
+export default SignupForm;
