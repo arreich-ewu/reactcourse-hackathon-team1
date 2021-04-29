40d3442bd20a039febd666f7cc4c3fd2827f7e04
arreich-ewu <arreich@eagles.ewu.edu>
2021-04-29 10:38:20 +0200

    added requests for films and images to App.js

--- qacinemas/src/App.js
+++ qacinemas/src/App.js
@@ -9,9 +9,44 @@ import Home from './Home/Home';
 import axios from 'axios';
 
 const FILMSURL = `http://localhost:4000/films`;
+const IMAGESURL = `http://localhost:4000/images`;
 
 
 function App() {
+  const [getError, setGetError] = useState(``);
+  const [films, setFilms] = useState(``);
+  const [images, setImages] = useState(``);
+
+  useEffect(() => {
+    const getData= async () => {
+      setFilms(await getFilms());
+      setImages(await getImages());
+    }
+    getData();
+  });
+
+  const getFilms = () => {
+    try {
+      const res = await axios.get(FILMSURL);
+      return res.data.length ? { films: res.data } : { error: 'There are no films stored' };
+    }
+    catch (e) {
+      setGetError(`Data not available from server: ${e.message}`);
+      return { error: `Data not available from server: ${e.message}` };
+    }
+  }
+
+  const getImages = () => {
+    try {
+      const res = await axios.get(IMAGESURL);
+      return res.data.length ? { images: res.data } : { error: 'There are no films stored' };
+    }
+    catch (e) {
+      setGetError(`Data not available from server: ${e.message}`);
+      return { error: `Data not available from server: ${e.message}` };
+    }
+  }
+
   return (
     <div className="container">
       <Header/>
