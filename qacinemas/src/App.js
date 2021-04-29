46ca33ed1abf8991c51b09dac823e09136e0132a
arreich-ewu <arreich@eagles.ewu.edu>
2021-04-29 10:29:30 +0200

    added logic for json-server to App.js

--- qacinemas/src/App.js
+++ qacinemas/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import SignupForm from './Components/SignupForm';
 import './qacinemas.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Home from './Home/Home';
+import axios from 'axios';
+
+const FILMSURL = `http://localhost:4000/films`;
 
 
 function App() {
@@ -13,28 +16,9 @@ function App() {
     <div className="container">
       <Header/>
       <div className="container">
+        <Home/>
       </div>
       <Footer />
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> f3a7c0c78a5cd89a8e5a37eb649bae1354202d45
     </div>
   );
 }
